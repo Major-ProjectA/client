fd9c0def4b3ec255ad65b4714a7bb4fad1b3896a
NgTheLuan <nguyenluan.work@gmail.com>
2021-04-25 18:20:53 +0700

    updated btn-LoadMore

--- src/components/Category/index.jsx
+++ src/components/Category/index.jsx
@@ -1,26 +1,47 @@
 import React, { useState, useEffect } from 'react';
 
-// import { useSelector } from 'react-redux';
-
 import categoryApi from '../../api/categoryAPI';
 
 const Category = () => {
   const [fetchCategories, setFetchCategories] = useState([]);
+  const [visible, setVisible] = useState([8]);
 
   useEffect(() => {
     const fetchCategories = async () => {
-      const params = {
-        _limit: 8,
-      };
-      const categorylist = await categoryApi.getAllCategory(params);
-      console.log(categorylist);
+      const categorylist = await categoryApi.getAllCategory();
+      // console.log(categorylist);
+
       setFetchCategories(categorylist);
     };
     fetchCategories();
   }, [categoryApi]);
 
-  // const jobs = useSelector((state) => state.jobs);
-  // console.log(jobs);
+  var categories = fetchCategories.slice(0, visible).map((listcategories) => {
+    return (
+      <p key={listcategories._id}>
+        <div className="col-md-3 col-sm-6 category-cus" style={{ float: 'left' }}>
+          <div className="category-box" data-aos="fade-up">
+            <div className="category-desc">
+              <div className="category-icon">
+                <img src={listcategories.career.icon} alt="" width="17%" />
+              </div>
+              <div className="category-detail category-desc-text">
+                <h4>
+                  <a href={'# '}>{listcategories.career.careerName}</a>
+                </h4>
+
+                <p>122 Jobs</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </p>
+    );
+  });
+
+  const handleSubmit = () => {
+    setVisible(visible + 4);
+  };
 
   return !fetchCategories.length ? (
     <div className="main-heading">
@@ -35,34 +56,17 @@ const Category = () => {
           <div className="main-heading">
             <h2>
               Browse Jobs By <span>Category</span>
+              <div>{categories}</div>
+              <div>
+                {visible < fetchCategories.length && (
+                  <a className="btn btn-primary" onClick={handleSubmit}>
+                    Load More
+                  </a>
+                )}
+              </div>
             </h2>
           </div>
         </div>
-        {fetchCategories.map((category) => (
-          <div key={category._id}>
-            <div className="col-md-3 col-sm-6 category-cus" style={{ float: 'left' }}>
-              <div className="category-box" data-aos="fade-up">
-                <div className="category-desc">
-                  <div className="category-icon">
-                    {/* <i className="icon-bargraph" aria-hidden="true"></i>
-                    <i
-                      className="icon-bargraph abs-icon"
-                      aria-hidden="true"
-                    ></i> */}
-                    <img src={category.career.icon} alt="" width="17%" />
-                  </div>
-                  <div className="category-detail category-desc-text">
-                    <h4>
-                      <a href={'# '}>{category.career.careerName}</a>
-                    </h4>
-
-                    <p>122 Jobs</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        ))}
       </div>
     </section>
   );
